4749ac40be6748ad101162b9c84c982ce17b20b8
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-04-16 23:58:35 -0400

    Add: Added CRUD for different functions my movie app will allow users to do.
    - Added the ability for a user to:
    1. Get a list of all the movies
    2. Read a movie by title
    3. Read a movie by genre
    4. Read a director by name
    5. Allow new users to register
    6. Allow users to update their usernames
    7. Allow a user to add a movie to their favorites list
    8. Allow a user to delete a movie from there favorites list.
    9. Allowed a user to delete their account.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
@@ -43,6 +44,208 @@ app.use(function (err, req, res, next) {
     console.error(err.stack)
     res.status(500).send('Something broke!')
 });
+
+// GET: Read list of movies
+app.get('/movies', (req, res) => {
+    res.status(200).json(movies);
+});
+
+// GET: Read a movie by title
+app.get('/movies/:title', (req, res) => {
+    const title = req.params.title;
+    const movie = movies.find(movie => movie.title === title);
+
+    if (!movie) {
+        return res.status(404).send({ error: 'Movie is not found' });
+    }
+
+    res.status(200).json(movie);
+});
+
+// GET: Read genre by name
+app.get('/genres/:name', (req, res) => {
+    const name = req.params.name;
+    const genre = genres.find(genre => genre.name === name);
+
+    if (!genre) {
+        return res.status(404).send({ error: 'Genre is not found' });
+    }
+
+    res.status(200).json(genre);
+});
+
+// GET: Read director by name
+app.get('/directors/:name', (req, res) => {
+    const name = req.params.name;
+    const director = directors.find(director => director.name === name);
+
+    if (!director) {
+        return res.status(404).send('Director not found');
+    }
+
+    res.status(200).json(director);
+});
+
+// POST: Allow New Users to Register
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+
+    // Check if the user's fullname is provided
+    if (newUser.fullname) {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+
+        res.status(201).json(newUser);
+    } else {
+        res.status(400).send('New user needs a name');
+    }
+});
+
+// PUT: Allow Users to Update Their Username
+app.put('/users/:userId', (req, res) => {
+    const userId = req.params.userId;
+    const updatedInfo = req.body;
+
+    // Find the user by id
+    const userToUpdate = users.find(user => user.id === userId);
+
+    // Check if the user exists
+    if (!userToUpdate) {
+        return res.status(404).send('User does not exist');
+    }
+
+    // Update the username if it is provided in the request body
+    if (updatedInfo.username) {
+        userToUpdate.username = updatedInfo.username;
+        res.status(200).json(userToUpdate);
+    } else {
+        return res.status(400).send('New username is required');
+    }
+});
+
+// POST: Allow a User to Add a Movie to Their Favorites 
+app.post('/users/:userId/favorites', (req, res) => {
+    const userId = req.params.userId;
+    const movieId = req.body.movieId;
+
+    // Find user by Id
+    const user = users.find(user => user.id === userId);
+
+    // See if the user exists
+    if (!user) {
+        return res.status(404).send('User does not exist');
+    }
+
+    // Check if movie Id is provided
+    if (!movieId) {
+        return res.status(400).send('Movie is required');
+    }
+
+    // Check if the movie is already in favorites
+    if (user.favorites.includes(movieId)) {
+        return res.status(400).send('Movie is already in favorites');
+    }
+
+    // Add new movie to the user's favorites list
+    user.favorites.push(movieId);
+
+    res.status(200).send('Movie has been added to favorites');
+});
+
+// DELETE: Allow Users to Remove a Movie from Their Favorites
+app.delete('/users/:userId/favorites/:movieId', (req, res) => {
+    const userId = req.params.userId;
+    const movieId = req.params.movieId;
+
+    // Find the user by ID
+    const user = users.find(user => user.id === userId);
+
+    // Check if the user exists
+    if (!user) {
+        return res.status(404).send('User does not exist');
+    }
+
+    // Check if the movie is in the user's favorites list
+    const movieIndex = user.favorites.indexOf(movieId);
+    if (movieIndex === -1) {
+        return res.status(400).send('Movie is not in favorites');
+    }
+
+    // Remove the movie from the user's favorites list
+    user.favorites.splice(movieIndex, 1);
+
+    res.status(200).send('Movie has been removed from favorites');
+});
+
+// Delete a user
+app.delete('/users/:id/', (req, res) => {
+    // Extract the user ID from the request parameters
+    const { id } = req.params;
+
+    // Find the index of the user in the users array
+    const userIndex = users.findIndex(user => user.id == id);
+
+    // Check if the user exists
+    if (userIndex !== -1) {
+        // Remove the user from the users array
+        users.splice(userIndex, 1);
+        // Log the updated users array
+        console.log(users);
+        // Send a success response
+        res.status(200).send('User has been deleted');
+    } else {
+        // Send a 404 response if the user is not found
+        res.status(404).send('User not found');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
