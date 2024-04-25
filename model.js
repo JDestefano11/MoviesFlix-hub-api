a7ff589630a8b3e549b6787c924b12ca61b1b846
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-04-25 16:25:11 -0400

    feat: Add movie and user schemas and modules
    - Created movie and user schemas to represent data from MongoDB database
    - Implemented separate modules for each schema
    - Exported modules and schemas for use in index.js

---
+++ model.js
@@ -0,0 +1,36 @@
+// Movie Schema
+const movieSchema = mongoose.Schema({
+    Title: { type: String, required: true },
+    Description: { type: String, required: true },
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Occupation: String,
+        BirthDate: Date,
+        BirthPlace: String,
+        Bio: String
+    },
+});
+
+// User Schema
+const userSchema = mongoose.Schema({
+    Username: { type: String, required: true },
+    Name: String,
+    Birthday: Date,
+    FavoriteMovies: [String]
+});
+
+
+// Modules
+const Movie = mongoose.model('Movie', movieSchema);
+const User = mongoose.model('User', userSchema);
+
+
+// Export modules & schemas
+module.exports = {
+    Movie: Movie,
+    User: User
+};
