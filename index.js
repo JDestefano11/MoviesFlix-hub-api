412127de2f3076ab88bdadcd5a962f952328cc3c
JDestefanoCodes <158540954+JDestefanoCodes@users.noreply.github.com>
2024-06-03 11:36:35 -0700

    Removed CORS

--- index.js
+++ index.js
@@ -47,22 +47,11 @@ app.use(passport.initialize());
 
 let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080', 'https://moviesflix-hub-fca46ebf9888.herokuapp.com/'];
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
-}));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 });
-
 
 
 const auth = require('./auth');
