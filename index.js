0e92cc2ecc90c8d0c6d7bd4d81d27641d9cfa0ef
JDestefanoCodes <158540954+JDestefanoCodes@users.noreply.github.com>
2024-06-04 10:45:05 -0700

    Remove unnecessary `router` variable declaration in auth.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.use((req, res, next) => {
 
 app.use(cors(corsOptions));
 
-const auth = require('./auth')(app);
+const auth = require('./auth');
 app.use('/auth', auth)
 
 // Define a route handler for the root endpoint
