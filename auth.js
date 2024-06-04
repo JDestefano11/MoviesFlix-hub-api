f4617aab94d6724774562d2fad0b4243cd67084c
JDestefanoCodes <158540954+JDestefanoCodes@users.noreply.github.com>
2024-06-04 11:20:29 -0700

    Added login

--- auth.js
+++ auth.js
@@ -1,19 +1,17 @@
+const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-const jwtSecret = require('crypto').randomBytes(32).toString('hex');
-const express = require('express');
+const { User } = require('./models.js');
+const jwtSecret = 'ThisIsATemporarySecretKey123'
+
+
+
 const router = express.Router();
 
-// Define the login route
-router.post('/login', passport.authenticate('local', { session: false }), async (req, res) => {
-    try {
-        const { _id, username } = req.user;
-        const token = jwt.sign({ id: _id, username }, process.env.JWT_SECRET);
-        res.json({ token, username });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+router.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
+    const { _id, username } = req.user;
+    const token = jwt.sign({ id: _id, username }, jwtSecret);
+    res.json({ token, username }); // Include username in the response
 });
 
-module.exports = router; // Export the router
+module.exports = router;
