b0b5d7bdef72a94abea88094e8077e2e6e9b7b05
JDestefanoCodes <158540954+JDestefanoCodes@users.noreply.github.com>
2024-06-04 11:08:05 -0700

    "Refactor auth.js: wrap router in module.exports function, add try-catch block to login route, and use process.env.JWT_SECRET instead of generating a secret"

--- auth.js
+++ auth.js
@@ -1,17 +1,19 @@
 
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-const { User } = require('./models.js');
 const jwtSecret = require('crypto').randomBytes(32).toString('hex');
 const express = require('express');
 
 
-const router = express.Router();
-
-router.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
-    const { _id, username } = req.user;
-    const token = jwt.sign({ id: _id, username }, jwtSecret);
-    res.json({ token, username });
-});
-
-module.exports = router;
+module.exports = (router) => {
+    router.post('/login', passport.authenticate('local', { session: false }), async (req, res) => {
+        try {
+            const { _id, username } = req.user;
+            const token = jwt.sign({ id: _id, username }, process.env.JWT_SECRET);
+            res.json({ token, username });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
+    });
+}
