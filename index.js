f8f4454608f4a98bf4482d0ecd3abc52125a2928
JDestefanoCodes <158540954+JDestefanoCodes@users.noreply.github.com>
2024-06-07 23:37:22 -0700

    jwt auth

--- index.js
+++ index.js
@@ -19,15 +19,6 @@ if (!connectionUri) {
     process.exit(1);
 }
 
-const JWT_SECRET = process.env.JWT_SECRET;
-
-if (!JWT_SECRET) {
-    console.error("JWT secret is missing!");
-    process.exit(1);
-}
-
-
-
 
 mongoose.connect(connectionUri, {
     useNewUrlParser: true,
