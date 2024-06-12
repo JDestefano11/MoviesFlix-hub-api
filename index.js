261c4959f9d76c60403ebb8daaea57dbf8315eb1
JDestefanoCodes <158540954+JDestefanoCodes@users.noreply.github.com>
2024-06-11 18:23:23 -0700

    "Commented out authentication check in login route"

--- index.js
+++ index.js
@@ -66,9 +66,9 @@ app.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ username });
 
-        if (!user || !(await bcrypt.compare(password, user.password))) {
+        /* if (!user || !(await bcrypt.compare(password, user.password))) {
             return res.status(401).json({ message: 'Authentication failed' });
-        }
+        } */
         const token = JWT_SECRET;
         return res.status(200).json({ message: 'Login successful', token });
     }
